67ed3f8da8984e87fa9691856b898bafba8d7f90
Hendra Anggrian <hendraanggrian@gmail.com>
2023-05-23 09:04:18 -0500

    Prepare iCal, VSCode settings, and remove boilerplates

--- jvm-library/sample/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    kotlin("jvm") version libs.versions.kotlin
-    kotlin("kapt") version libs.versions.kotlin
-    application
-}
-
-application.mainClass.set("com.example.MyApp")
-
-dependencies.implementation(project(":library"))
